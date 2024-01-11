aa7ca0daf6fdf033591c53cd5cf0a2ba1c3e9f0c
Quoc Vinh <nguyenvinh22012002@gmail.com>
2024-01-11 09:25:10 +0700

    done create

--- force-app/main/default/lwc/lwcCreateStudent/lwcCreateStudent.js
+++ force-app/main/default/lwc/lwcCreateStudent/lwcCreateStudent.js
@@ -1,6 +1,8 @@
 // LWC_UpdateStudent.js
 import { LightningElement, api ,wire,track} from 'lwc';
 import getClassOptions from '@salesforce/apex/LWC_SearchStudentCtrl.getClassOptions';
+import createStudentRec from '@salesforce/apex/LWC_CreateStudentCtrl.createStudentRec';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class LWC_CreateStudent extends LightningElement {
     @track firstName = '';
@@ -8,12 +10,13 @@ export default class LWC_CreateStudent extends LightningElement {
     @track selectedClass = '';
     @track address = '';
     @track birthdate = '';
-    @track selectedGender = '';
+    @track selectedGender = false;
     @track firstNameError = '';
     @track lastNameError = '';
     @track classError = '';
     @track addressError = '';
     @track birthdateError = '';
+    @track genderError = '';
     @api genderOptions;
     classes;
     selectedClass;
@@ -70,89 +73,125 @@ export default class LWC_CreateStudent extends LightningElement {
         this.selectedGender = event.target.value;
     }
     handleCreate() {
-        // Perform validation before submitting the form
-        this.clearErrors();
-        try {
-            this.firstName = this.template.querySelector('[data-id="sFirstName"]').value;
-            this.lastName = this.template.querySelector('[data-id="sLastName"]').value;
-            this.address = this.template.querySelector('[data-id="sAddress"]').value;
-            this.birthdate = this.template.querySelector('[data-id="sBirthdate"]').value;
-            console.log(this.firstName);
-            console.log(this.lastName);
-            console.log(this.selectedClass);
-            console.log(this.address);
-            console.log(this.birthdate);
-            console.log(this.selectedGender);
-            
-            if (!this.firstName) {
-                this.firstNameError = 'Please enter First Name';
-            }
-            if(this.firstName){
-                this.firstNameError = '';
-
-            }
-            if (!this.lastName) {
-                this.lastNameError = 'Please enter Last Name';
-            }
-            else if (this.lastName) {
-                this.lastNameError = '';
-            }
-
-            if (!this.selectedClass) {
-                this.classError = 'Please select a Class';
-            }
-            else if (!this.selectedClass) {
-                this.classError = '';
-            }
-
-            if (!this.address) {
-                this.addressError = 'Please enter Address';
-            }
-            else if (this.address) {
-                this.addressError = '';
-            }
-            
-            if (!this.birthdate) {
-                this.birthdateError = 'Please enter Birthdate';
-            }
-            else if (this.birthdate) {
-                this.birthdateError = '';
-            }
-
-            if (this.selectedGender==null) {
-                this.genderError = 'Please select Gender';
-            }
-            else if (this.selectedGender) {
-                this.genderError = '';
-            }
-            // Check if there are any validation errors
-            if (
-                this.firstNameError ||
-                this.lastNameError ||
-                this.classError ||
-                this.addressError ||
-                this.birthdateError ||
-                this.genderError
-            ) {
-                // Validation failed, do not proceed with form submission
-                return;
-            }
-            console.log("validation success");
-
-        } catch (error) {
-            console.log("error when validation student", error.message);
+        if(!this.validation()) {
+            return;
         }
-        // Form submission logic goes here
+        this.createStudent();
+        this.resetForm();
     }
+    createStudent(){
+        console.log("this.firstName: ",this.firstName);
+        console.log("this.lastName: ",this.lastName);
+        console.log("this.selectedClass: ",this.selectedClass);
+        console.log("this.selectedGender: ",this.selectedGender);
+        console.log("this.address: ",this.address);
+        console.log("this.birthdate: ",this.birthdate);
+        createStudentRec({
+            //String  sFirstName, String sLastName, String sClassId, Boolean sGender, String sAddress, Date sBirthdate
+            sFirstName:this.firstName,
+            sLastName:this.lastName,
+            sClassId:this.selectedClass,
+            sGender:this.selectedGender,
+            sAddress:this.address,
+            sBirthdate:this.birthdate
+            
+        }).then(newSCode => {
+            this.showSuccessToast('Multiples Student deleted successfully', newSCode);
+        })
+        .catch(error => {
+            this.showSuccessToast('Error creating student record:', error);
+        });    
+       
+    }
+    validation(){
+        this.clearErrors();
+        this.firstName = this.template.querySelector('[data-id="sFirstName"]').value;
+        this.lastName = this.template.querySelector('[data-id="sLastName"]').value;
+        this.address = this.template.querySelector('[data-id="sAddress"]').value;
+        this.birthdate = this.template.querySelector('[data-id="sBirthdate"]').value;
+        if (!this.firstName) {
+            this.firstNameError = 'Please enter First Name';
+        }
+        if(this.firstName){
+            this.firstNameError = '';
 
+        }
+        if (!this.lastName) {
+            this.lastNameError = 'Please enter Last Name';
+        }
+        else if (this.lastName) {
+            this.lastNameError = '';
+        }
+
+        if (!this.selectedClass) {
+            this.classError = 'Please select a Class';
+        }
+        else if (!this.selectedClass) {
+            this.classError = '';
+        }
+
+        if (!this.address) {
+            this.addressError = 'Please enter Address';
+        }
+        else if (this.address) {
+            this.addressError = '';
+        }
+        if (this.selectedGender==null) {
+            this.genderError = 'Please select Gender';
+        }
+        else if (this.selectedGender) {
+            this.genderError = '';
+        }
+        if (!this.birthdate) {
+            this.birthdateError = 'Please enter Birthdate';
+        }
+        else if (this.birthdate) {
+            var bd = new Date(this.birthdate);
+            var today = new Date();
+            var age = today.getFullYear() - bd.getFullYear();
+            if (today.getMonth() < bd.getMonth() || (today.getMonth() === bd.getMonth() && today.getDate() < bd.getDate())) {
+                age--;
+            }
+            if (age <= 17) {
+                this.birthdateError  = "Student must be atleast 17 years old";
+            }else{
+                this.birthdateError = '';
+
+            }
+        }
+        if (
+            this.firstNameError ||
+            this.lastNameError ||
+            this.classError ||
+            this.addressError ||
+            this.birthdateError ||
+            this.genderError
+        ) {
+            return false;
+        }
+        return true;
+    }
+    resetForm() {
+        console.log("reset form");
+        this.firstName = '';
+        this.lastName = '';
+        this.address = '';
+        this.birthdate = '';
+    }
     clearErrors() {
-        // Clear all error messages
-        console.log("clear all error messages");
         this.firstNameError = '';
         this.lastNameError = '';
         this.classError = '';
         this.addressError = '';
         this.birthdateError = '';
         this.genderError = '';
+    }
+    showSuccessToast(message) {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: message,
+            variant: 'success',
+        });
+        this.dispatchEvent(event);
     }
 }
--- force-app/main/default/lwc/lwcUpdateStudent/lwcUpdateStudent.js
+++ force-app/main/default/lwc/lwcUpdateStudent/lwcUpdateStudent.js
@@ -21,6 +21,121 @@ export default class LWC_UpdateStudent extends LightningElement {
             this.classes = undefined;
         }
     }
+    handleUpdate() {
+        // Perform validation before submitting the form
+        if(!this.validation()) {
+            return;
+        }
+        this.updateStudent();
+    }
+    updateStudent(){
+        console.log("this.firstName: ",this.firstName);
+        console.log("this.lastName: ",this.lastName);
+        console.log("this.selectedClass: ",this.selectedClass);
+        console.log("this.selectedGender: ",this.selectedGender);
+        console.log("this.address: ",this.address);
+        console.log("this.birthdate: ",this.birthdate);
+        updateStudentRec({
+            //String  sFirstName, String sLastName, String sClassId, Boolean sGender, String sAddress, Date sBirthdate
+            sFirstName:this.firstName,
+            sLastName:this.lastName,
+            sClassId:this.selectedClass,
+            sGender:this.selectedGender,
+            sAddress:this.address,
+            sBirthdate:this.birthdate
+            
+        }).then(newSCode => {
+            this.showSuccessToast('Multiples Student deleted successfully', newSCode);
+        })
+        .catch(error => {
+            this.showSuccessToast('Error creating student record:', error);
+        });    
+       
+    }
+    validation(){
+        this.clearErrors();
+        this.firstName = this.template.querySelector('[data-id="sFirstName"]').value;
+        this.lastName = this.template.querySelector('[data-id="sLastName"]').value;
+        this.address = this.template.querySelector('[data-id="sAddress"]').value;
+        this.birthdate = this.template.querySelector('[data-id="sBirthdate"]').value;
+        if (!this.firstName) {
+            this.firstNameError = 'Please enter First Name';
+        }
+        if(this.firstName){
+            this.firstNameError = '';
+
+        }
+        if (!this.lastName) {
+            this.lastNameError = 'Please enter Last Name';
+        }
+        else if (this.lastName) {
+            this.lastNameError = '';
+        }
+
+        if (!this.selectedClass) {
+            this.classError = 'Please select a Class';
+        }
+        else if (!this.selectedClass) {
+            this.classError = '';
+        }
+
+        if (!this.address) {
+            this.addressError = 'Please enter Address';
+        }
+        else if (this.address) {
+            this.addressError = '';
+        }
+        if (this.selectedGender==null) {
+            this.genderError = 'Please select Gender';
+        }
+        else if (this.selectedGender) {
+            this.genderError = '';
+        }
+        if (!this.birthdate) {
+            this.birthdateError = 'Please enter Birthdate';
+        }
+        else if (this.birthdate) {
+            var bd = new Date(this.birthdate);
+            var today = new Date();
+            var age = today.getFullYear() - bd.getFullYear();
+            if (today.getMonth() < bd.getMonth() || (today.getMonth() === bd.getMonth() && today.getDate() < bd.getDate())) {
+                age--;
+            }
+            if (age <= 17) {
+                this.birthdateError  = "Student must be atleast 17 years old";
+            }else{
+                this.birthdateError = '';
+
+            }
+        }
+        if (
+            this.firstNameError ||
+            this.lastNameError ||
+            this.classError ||
+            this.addressError ||
+            this.birthdateError ||
+            this.genderError
+        ) {
+            return false;
+        }
+        return true;
+    }
+    clearErrors() {
+        this.firstNameError = '';
+        this.lastNameError = '';
+        this.classError = '';
+        this.addressError = '';
+        this.birthdateError = '';
+        this.genderError = '';
+    }
+    showSuccessToast(message) {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: message,
+            variant: 'success',
+        });
+        this.dispatchEvent(event);
+    }
     // handleUpdate() {
     //     // Emit an event to notify the parent component to open the modal
     //     const updateEvent = new CustomEvent('openmodal', {
